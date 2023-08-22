25241ea7369b31e175366c5622d1e8c4eaf79415
RMerkulov <fotoromario@gmail.com>
2023-08-22 20:51:11 +0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import {Eva} from "@eva-ics/webengine";
-import {useEvaState} from "@eva-ics/webengine-react";
 import {Gauge} from "./components/gauge/index";
-import {GaugeType} from "./components/gauge/types";
 
 
 const App = ({engine}: { engine: Eva }) => {
