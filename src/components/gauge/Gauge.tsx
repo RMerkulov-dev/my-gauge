58479cf55a6279df13967c612e113ec8e09bb1ba
RMerkulov <fotoromario@gmail.com>
2023-08-22 13:49:07 +0300

    sml fix

--- src/components/gauge/Gauge.tsx
+++ src/components/gauge/Gauge.tsx
@@ -64,7 +64,6 @@ const Gauge = ({
                     setProgressColorOfValue(ClassNameColors.YELLOW);
                     break;
                 case value >= critValue:
-                    console.log("true", value)
                     setProgressColorOfValue(ClassNameColors.RED);
                     break;
                 default:
