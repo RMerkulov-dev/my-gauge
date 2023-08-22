15cac56f66b70e5c64e17924bda3fff423a19e4b
RMerkulov <fotoromario@gmail.com>
2023-08-22 21:35:33 +0300

    refactor

--- src/components/gauge/Gauge.tsx
+++ src/components/gauge/Gauge.tsx
@@ -4,7 +4,28 @@ import {GaugeConstructor, GaugeType} from "./types";
 import {GaugeLight, GaugeMinimal, GaugeSphere, GaugeStandard} from "./index";
 
 
-const Gauge = ({type = GaugeType.STANDARD, minValue, maxValue, engine}: GaugeConstructor) => {
+const Gauge = ({
+                   type = GaugeType.STANDARD,
+                   minValue,
+                   maxValue,
+                   warnValue,
+                   critValue,
+                   engine,
+                   value,
+                   diameter,
+                   startAngle,
+                   endAngle,
+                   numTicks,
+                   offset,
+                   arcStrokeWidth,
+                   strokeLineCap,
+                   tickLength,
+                   baseRadius,
+                   tipRadius,
+                   middleRadius,
+                   needleOffset,
+
+               }: GaugeConstructor) => {
     const GAUGES = {
         [GaugeType.STANDARD]: GaugeStandard,
         [GaugeType.SPHERE]: GaugeSphere,
@@ -13,12 +34,16 @@ const Gauge = ({type = GaugeType.STANDARD, minValue, maxValue, engine}: GaugeCon
     };
 
     const state = useEvaState({oid: "sensor:tests/temp", engine});
-    const value = state.value;
+    const stateValue = state.value;
 
     const SelectedGauge = GAUGES[type];
 
     return (
-        <SelectedGauge engine={engine} value={value} maxValue={maxValue} minValue={minValue}/>
+        <SelectedGauge engine={engine} value={stateValue} maxValue={maxValue} minValue={minValue} diameter={diameter}
+                       warnValue={warnValue} critValue={critValue} startAngle={startAngle} endAngle={endAngle}
+                       numTicks={numTicks} offset={offset} arcStrokeWidth={arcStrokeWidth} strokeLineCap={strokeLineCap}
+                       tickLength={tickLength} baseRadius={baseRadius} tipRadius={tipRadius} middleRadius={middleRadius}
+                       needleOffset={needleOffset}/>
     );
 };
 
