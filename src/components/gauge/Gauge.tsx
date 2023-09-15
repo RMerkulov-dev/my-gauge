fb9de562a8a9e4906b772a3b0f7302d3e379cdf3
RMerkulov <fotoromario@gmail.com>
2023-09-15 11:55:39 +0300

    upd gauges

--- src/components/gauge/Gauge.tsx
+++ src/components/gauge/Gauge.tsx
@@ -1,119 +1,165 @@
-import React from 'react';
-import {ItemValueThreshold, useEvaState} from "@eva-ics/webengine-react";
-import {GaugeLight, GaugeMinimal, GaugeSphere, GaugeStandard} from "./index";
-import {Eva} from "@eva-ics/webengine";
-
-enum StrokeLineCamp {
-    BUTT = "butt",
-    ROUND = "round",
-    SQUARE = "square",
-    INHERIT = 'inherit'
-}
-
-enum GaugeType {
-    STANDARD = "standard",
-    SPHERE = "sphere",
-    MINIMAL = "minimal",
-    LIGHT = "light"
-}
-
-interface GaugeParams {
-    oid: string;
-    minValue: number,
-    maxValue: number,
-    type?: "standard" | "sphere" | "minimal" | "light";
-    engine?: Eva;
-    value?: number;
-    digits?: number;
-    units?: number;
-    threshold?: Array<ItemValueThreshold>;
-    format_with?: (value: any) => any;
-    diameter?: number,
-    warnValue?: number;
-    critValue?: number;
-    startAngle?: number,
-    endAngle?: number,
-    numTicks?: number,
-    offset?: number,
-    arcStrokeWidth?: number,
-    strokeLineCap?: StrokeLineCamp | undefined,
-    tickLength?: number,
-    baseRadius?: number,
-    tipRadius?: number,
-    needleOffset?: number,
-    middleRadius?: number,
-    showValue?: boolean,
-}
+import { useEvaState } from "@eva-ics/webengine-react";
+import {
+  GaugeLight,
+  GaugeMinimal,
+  GaugeParams,
+  GaugeSphere,
+  GaugeStandard,
+  GaugeType,
+} from "./index";
 
 const Gauge = ({
-                   oid,
-                   minValue,
-                   maxValue,
-                   type,
-                   engine,
-                   digits,
-                   units,
-                   threshold,
-                   format_with,
-                   diameter,
-                   warnValue,
-                   critValue,
-                   startAngle,
-                   endAngle,
-                   numTicks,
-                   offset,
-                   arcStrokeWidth,
-                   strokeLineCap,
-                   tickLength,
-                   baseRadius,
-                   tipRadius,
-                   needleOffset,
-                   middleRadius,
-                   showValue
-               }: GaugeParams) => {
+  oid,
+  minValue,
+  maxValue,
+  type,
+  engine,
+  digits,
+  units,
+  threshold,
+  format_with,
+  diameter,
+  warnValue,
+  critValue,
+  startAngle,
+  endAngle,
+  numTicks,
+  offset,
+  arcStrokeWidth,
+  strokeLineCap,
+  tickLength,
+  baseRadius,
+  tipRadius,
+  needleOffset,
+  middleRadius,
+  showValue,
+  label,
+}: GaugeParams) => {
+  const state = useEvaState({ oid, engine });
+  const { value } = state;
 
-    const state = useEvaState({oid, engine});
-    const {value} = state;
-
-    switch (type) {
-        case GaugeType.SPHERE:
-            return <GaugeSphere oid={oid} value={value} minValue={minValue} maxValue={maxValue} engine={engine}
-                                digits={digits}
-                                units={units} threshold={threshold} format_with={format_with} diameter={diameter}
-                                warnValue={warnValue} critValue={critValue} startAngle={startAngle} endAngle={endAngle}
-                                numTicks={numTicks} offset={offset} arcStrokeWidth={arcStrokeWidth}
-                                strokeLineCap={strokeLineCap} tickLength={tickLength} baseRadius={baseRadius}
-                                tipRadius={tipRadius} needleOffset={needleOffset} middleRadius={middleRadius}
-                                showValue={showValue}/>;
-        case GaugeType.LIGHT:
-            return <GaugeLight oid={oid} value={value} minValue={minValue} maxValue={maxValue} engine={engine}
-                               digits={digits}
-                               units={units} threshold={threshold} format_with={format_with} diameter={diameter}
-                               warnValue={warnValue} critValue={critValue} startAngle={startAngle} endAngle={endAngle}
-                               numTicks={numTicks} offset={offset} arcStrokeWidth={arcStrokeWidth}
-                               strokeLineCap={strokeLineCap} tickLength={tickLength} baseRadius={baseRadius}
-                               tipRadius={tipRadius} needleOffset={needleOffset} middleRadius={middleRadius}
-                               showValue={showValue}/>;
-        case GaugeType.MINIMAL:
-            return <GaugeMinimal oid={oid} value={value} minValue={minValue} maxValue={maxValue} engine={engine}
-                                 digits={digits}
-                                 units={units} threshold={threshold} format_with={format_with} diameter={diameter}
-                                 warnValue={warnValue} critValue={critValue} startAngle={startAngle} endAngle={endAngle}
-                                 numTicks={numTicks} offset={offset} arcStrokeWidth={arcStrokeWidth}
-                                 strokeLineCap={strokeLineCap} tickLength={tickLength} baseRadius={baseRadius}
-                                 tipRadius={tipRadius} needleOffset={needleOffset} middleRadius={middleRadius}
-                                 showValue={showValue}/>;
-        default:
-            return <GaugeStandard oid={oid} value={value} minValue={minValue} maxValue={maxValue} engine={engine}
-                                  digits={digits}
-                                  units={units} threshold={threshold} format_with={format_with} diameter={diameter}
-                                  warnValue={warnValue} critValue={critValue} startAngle={startAngle}
-                                  endAngle={endAngle}
-                                  numTicks={numTicks} offset={offset} arcStrokeWidth={arcStrokeWidth}
-                                  strokeLineCap={strokeLineCap} tickLength={tickLength} baseRadius={baseRadius}
-                                  tipRadius={tipRadius} needleOffset={needleOffset} middleRadius={middleRadius}
-                                  showValue={showValue}/>;
-    }
+  switch (type) {
+    case GaugeType.Sphere:
+      return (
+        <GaugeSphere
+          oid={oid}
+          value={value}
+          minValue={minValue}
+          maxValue={maxValue}
+          engine={engine}
+          digits={digits}
+          units={units}
+          threshold={threshold}
+          format_with={format_with}
+          diameter={diameter}
+          warnValue={warnValue}
+          critValue={critValue}
+          startAngle={startAngle}
+          endAngle={endAngle}
+          numTicks={numTicks}
+          offset={offset}
+          arcStrokeWidth={arcStrokeWidth}
+          strokeLineCap={strokeLineCap}
+          tickLength={tickLength}
+          baseRadius={baseRadius}
+          tipRadius={tipRadius}
+          needleOffset={needleOffset}
+          middleRadius={middleRadius}
+          showValue={showValue}
+          label={label}
+        />
+      );
+    case GaugeType.Light:
+      return (
+        <GaugeLight
+          oid={oid}
+          value={value}
+          minValue={minValue}
+          maxValue={maxValue}
+          engine={engine}
+          digits={digits}
+          units={units}
+          threshold={threshold}
+          format_with={format_with}
+          diameter={diameter}
+          warnValue={warnValue}
+          critValue={critValue}
+          startAngle={startAngle}
+          endAngle={endAngle}
+          numTicks={numTicks}
+          offset={offset}
+          arcStrokeWidth={arcStrokeWidth}
+          strokeLineCap={strokeLineCap}
+          tickLength={tickLength}
+          baseRadius={baseRadius}
+          tipRadius={tipRadius}
+          needleOffset={needleOffset}
+          middleRadius={middleRadius}
+          showValue={showValue}
+          label={label}
+        />
+      );
+    case GaugeType.Minimal:
+      return (
+        <GaugeMinimal
+          oid={oid}
+          value={value}
+          minValue={minValue}
+          maxValue={maxValue}
+          engine={engine}
+          digits={digits}
+          units={units}
+          threshold={threshold}
+          format_with={format_with}
+          diameter={diameter}
+          warnValue={warnValue}
+          critValue={critValue}
+          startAngle={startAngle}
+          endAngle={endAngle}
+          numTicks={numTicks}
+          offset={offset}
+          arcStrokeWidth={arcStrokeWidth}
+          strokeLineCap={strokeLineCap}
+          tickLength={tickLength}
+          baseRadius={baseRadius}
+          tipRadius={tipRadius}
+          needleOffset={needleOffset}
+          middleRadius={middleRadius}
+          showValue={showValue}
+          label={label}
+        />
+      );
+    default:
+      return (
+        <GaugeStandard
+          oid={oid}
+          value={value}
+          minValue={minValue}
+          maxValue={maxValue}
+          engine={engine}
+          digits={digits}
+          units={units}
+          threshold={threshold}
+          format_with={format_with}
+          diameter={diameter}
+          warnValue={warnValue}
+          critValue={critValue}
+          startAngle={startAngle}
+          endAngle={endAngle}
+          numTicks={numTicks}
+          offset={offset}
+          arcStrokeWidth={arcStrokeWidth}
+          strokeLineCap={strokeLineCap}
+          tickLength={tickLength}
+          baseRadius={baseRadius}
+          tipRadius={tipRadius}
+          needleOffset={needleOffset}
+          middleRadius={middleRadius}
+          showValue={showValue}
+          label={label}
+        />
+      );
+  }
 };
 
 export default Gauge;
--- src/components/gauge/GaugeSphere.tsx
+++ src/components/gauge/GaugeSphere.tsx
@@ -1,224 +1,187 @@
-import React, {useEffect, useState} from 'react';
-import {useGauge} from './index';
-import {ItemValue, ItemValueThreshold} from "@eva-ics/webengine-react";
-import {Eva} from "@eva-ics/webengine";
-
-
-enum StrokeLineCamp {
-    BUTT = "butt",
-    ROUND = "round",
-    SQUARE = "square",
-    INHERIT = 'inherit'
-}
-
-enum ClassNameColors {
-    GREEN = "progress-color",
-    YELLOW = "warning-progress-color",
-    RED = "critical-progress-color",
-    TICK = "tick-color",
-    NEEDLE = "needle-color"
-}
-
-enum GaugeType {
-    STANDARD = "standard",
-    SPHERE = "sphere",
-    MINIMAL = "minimal",
-    LIGHT = "light"
-}
-
-
-interface GaugeParams {
-    oid: string;
-    minValue: number,
-    maxValue: number,
-    type?: GaugeType;
-    engine?: Eva;
-    value?: number;
-    digits?: number;
-    units?: number;
-    threshold?: Array<ItemValueThreshold>;
-    format_with?: (value: any) => any;
-    diameter?: number,
-    warnValue?: number;
-    critValue?: number;
-    startAngle?: number,
-    endAngle?: number,
-    numTicks?: number,
-    offset?: number,
-    arcStrokeWidth?: number,
-    strokeLineCap?: StrokeLineCamp | undefined,
-    tickLength?: number,
-    baseRadius?: number,
-    tipRadius?: number,
-    needleOffset?: number,
-    middleRadius?: number,
-    showValue?: boolean,
-}
+import React, { useEffect, useState } from "react";
+import { ClassNameColors, GaugeParams, StrokeLineCap } from "./index";
+import { ItemValue } from "@eva-ics/webengine-react";
+import { useGauge } from "./common.tsx";
 
 const options = {
-    value: 0, // Indicator value
-    diameter: 200, // GaugeStandard diameter value
-    minValue: 0, // Minimum value indicator
-    maxValue: 100, // Maximum value indicator
-    startAngle: 45, // Initial indicator position
-    endAngle: 315, // End indicator position
-    numTicks: 5, // Step of indicator values
-    offset: -50, // Distance of indicator line from the center
-    arcStrokeWidth: 24, // Indicator line thickness
-    strokeLineCap: StrokeLineCamp.BUTT, // Type of progress line
-    tickLength: 10, // Length of ticks
-    baseRadius: 6, // Radius of central point of arrow indicator
-    middleRadius: 14, //Radius of middle circle of arrow indicator
-    tipRadius: 2, // Radius of end point of arrow indicator
-    needleOffset: 10, // Length of arrow indicator
+  value: 0, // Indicator value
+  diameter: 200, // GaugeStandard diameter value
+  minValue: 0, // Minimum value indicator
+  maxValue: 100, // Maximum value indicator
+  startAngle: 45, // Initial indicator position
+  endAngle: 315, // End indicator position
+  numTicks: 5, // Step of indicator values
+  offset: -50, // Distance of indicator line from the center
+  arcStrokeWidth: 24, // Indicator line thickness
+  strokeLineCap: StrokeLineCap, // Type of progress line
+  tickLength: 10, // Length of ticks
+  baseRadius: 6, // Radius of central point of arrow indicator
+  middleRadius: 14, //Radius of middle circle of arrow indicator
+  tipRadius: 2, // Radius of end point of arrow indicator
+  needleOffset: 10, // Length of arrow indicator
 };
 
 const GaugeSphere = ({
-                         oid,
-                         minValue,
-                         maxValue,
-                         warnValue,
-                         critValue,
-                         engine,
-                         digits,
-                         units,
-                         threshold,
-                         format_with,
-                         showValue,
-                         value = options.value,
-                         diameter = options.diameter,
-                         startAngle = options.startAngle,
-                         endAngle = options.endAngle,
-                         numTicks = options.numTicks,
-                         offset = options.offset,
-                         arcStrokeWidth = options.arcStrokeWidth,
-                         strokeLineCap = options.strokeLineCap,
-                         tickLength = options.tickLength,
-                         baseRadius = options.baseRadius,
-                         tipRadius = options.tipRadius,
-                         needleOffset = options.needleOffset,
-                         middleRadius = options.middleRadius,
+  oid,
+  minValue,
+  maxValue,
+  warnValue,
+  critValue,
+  engine,
+  digits,
+  units,
+  threshold,
+  format_with,
+  showValue,
+  label,
+  value = options.value,
+  diameter = options.diameter,
+  startAngle = options.startAngle,
+  endAngle = options.endAngle,
+  numTicks = options.numTicks,
+  offset = options.offset,
+  arcStrokeWidth = options.arcStrokeWidth,
+  strokeLineCap = StrokeLineCap.Butt,
+  tickLength = options.tickLength,
+  baseRadius = options.baseRadius,
+  tipRadius = options.tipRadius,
+  needleOffset = options.needleOffset,
+  middleRadius = options.middleRadius,
+}: GaugeParams) => {
+  const [progressColorOfValue, setProgressColorOfValue] = useState(
+    ClassNameColors.Green
+  );
 
-                     }: GaugeParams) => {
-    const [progressColorOfValue, setProgressColorOfValue] = useState(ClassNameColors.GREEN);
+  if (value > maxValue) {
+    value = maxValue;
+  }
 
-    if (value > maxValue) {
-        value = maxValue
+  useEffect(() => {
+    if (warnValue === undefined && critValue === undefined) {
+      setProgressColorOfValue(ClassNameColors.Green);
+    } else if (critValue === undefined) {
+      setProgressColorOfValue(
+        value < warnValue! ? ClassNameColors.Green : ClassNameColors.Yellow
+      );
+    } else if (warnValue === undefined) {
+      setProgressColorOfValue(
+        value < critValue ? ClassNameColors.Green : ClassNameColors.Red
+      );
+    } else {
+      switch (true) {
+        case value >= minValue && value < warnValue:
+          setProgressColorOfValue(ClassNameColors.Green);
+          break;
+        case value > warnValue && value < critValue:
+          setProgressColorOfValue(ClassNameColors.Yellow);
+          break;
+        case value >= critValue:
+          setProgressColorOfValue(ClassNameColors.Red);
+          break;
+        default:
+          return;
+      }
     }
+  }, [value, warnValue, critValue, minValue, maxValue, progressColorOfValue]);
 
-    useEffect(() => {
-        if (warnValue === undefined && critValue === undefined) {
-            setProgressColorOfValue(ClassNameColors.GREEN);
-        } else if (critValue === undefined) {
-            // @ts-ignore
-            setProgressColorOfValue(value < warnValue ? ClassNameColors.GREEN : ClassNameColors.YELLOW);
-        } else if (warnValue === undefined) {
-            setProgressColorOfValue(value < critValue ? ClassNameColors.GREEN : ClassNameColors.RED);
-        } else {
-            switch (true) {
-                case value >= minValue && value < warnValue:
-                    setProgressColorOfValue(ClassNameColors.GREEN);
-                    break;
-                case value > warnValue && value < critValue:
-                    setProgressColorOfValue(ClassNameColors.YELLOW);
-                    break;
-                case value >= critValue:
+  const {
+    ticks,
+    getTickProps,
+    getLabelProps,
+    valueToAngle,
+    angleToValue,
+    getArcProps,
+    getNeedleProps,
+    getSVGProps,
+  } = useGauge({
+    startAngle,
+    endAngle,
+    numTicks,
+    diameter,
+    domain: [minValue, maxValue],
+  });
 
-                    setProgressColorOfValue(ClassNameColors.RED);
-                    break;
-                default:
-                    return;
-            }
-        }
-    }, [value, warnValue, critValue, minValue, maxValue, progressColorOfValue]);
+  const { tip, base, points } = getNeedleProps({
+    value,
+    baseRadius,
+    tipRadius,
+    offset: needleOffset,
+  });
 
-
-    const {
-        ticks,
-        getTickProps,
-        getLabelProps,
-        valueToAngle,
-        angleToValue,
-        getArcProps,
-        getNeedleProps,
-        getSVGProps,
-    } = useGauge({
-        startAngle,
-        endAngle,
-        numTicks,
-        diameter,
-        domain: [minValue, maxValue],
-    });
-
-    const {tip, base, points} = getNeedleProps({
-        value,
-        baseRadius,
-        tipRadius,
-        offset: needleOffset,
-    });
-
-    return (
-        <div className="gauge-container">
-            <div className="gauge-wrapper">
-                <svg {...getSVGProps()} className="gauge-preview">
-                    <path
-                        {...getArcProps({
-                            offset,
-                            startAngle,
-                            endAngle,
-                        })}
-                        fill="none"
-                        className="progress-background-color"
-                        strokeWidth={arcStrokeWidth}
-                        strokeLinecap={strokeLineCap}
-                    />
-                    {value > minValue && (
-                        <path
-                            {...getArcProps({
-                                offset,
-                                startAngle,
-                                endAngle: valueToAngle(value),
-                            })}
-                            fill="none"
-                            className={progressColorOfValue}
-                            strokeWidth={arcStrokeWidth}
-                            strokeLinecap={strokeLineCap}
-                        />
-                    )}
-                    <g id="ticks">
-                        {ticks.map((angle) => (
-                            <React.Fragment key={`tick-group-${angle}`}>
-                                <line
-                                    className={ClassNameColors.TICK}
-                                    {...getTickProps({angle, length: tickLength})}
-                                />
-                                <text
-                                    className="text-default-color"
-                                    {...getLabelProps({angle, offset: 20})}
-                                >
-                                    {angleToValue(angle)}
-                                </text>
-                            </React.Fragment>
-                        ))}
-                    </g>
-                    <g id="needle">
-                        <circle className="middle-base-color" {...base} r={middleRadius}/>
-                        <circle className={ClassNameColors.NEEDLE} {...base} />
-                        <circle className={ClassNameColors.NEEDLE} {...tip} />
-                        <polyline className={ClassNameColors.NEEDLE} points={points}/>
-                        <circle className="midpoint-color" {...base} r={4}/>
-                    </g>
-                </svg>
-                <div className="gauge-value">
-                    {showValue &&
-                        // @ts-ignore
-                        <ItemValue engine={engine} oid={oid} digits={digits} units={units} threshold={threshold}
-                                   format_with={format_with}/>}
-                </div>
-            </div>
-
+  return (
+    <div className="gauge-container">
+      <div className="gauge-wrapper">
+        <svg {...getSVGProps()} className="gauge-preview">
+          <path
+            {...getArcProps({
+              offset,
+              startAngle,
+              endAngle,
+            })}
+            fill="none"
+            className="gauge-progress-background-color"
+            strokeWidth={arcStrokeWidth}
+            strokeLinecap={strokeLineCap}
+          />
+          {value > minValue && (
+            <path
+              {...getArcProps({
+                offset,
+                startAngle,
+                endAngle: valueToAngle(value),
+              })}
+              fill="none"
+              className={progressColorOfValue}
+              strokeWidth={arcStrokeWidth}
+              strokeLinecap={strokeLineCap}
+            />
+          )}
+          <g id="ticks">
+            {ticks.map((angle) => (
+              <React.Fragment key={`tick-group-${angle}`}>
+                <line
+                  className={ClassNameColors.Tick}
+                  {...getTickProps({ angle, length: tickLength })}
+                />
+                <text
+                  className="gauge-text-default-color"
+                  {...getLabelProps({ angle, offset: 20 })}
+                >
+                  {angleToValue(angle)}
+                </text>
+              </React.Fragment>
+            ))}
+          </g>
+          <g id="needle">
+            <circle
+              className="gauge-middle-base-color"
+              {...base}
+              r={middleRadius}
+            />
+            <circle className={ClassNameColors.Needle} {...base} />
+            <circle className={ClassNameColors.Needle} {...tip} />
+            <polyline className={ClassNameColors.Needle} points={points} />
+            <circle className="gauge-midpoint-color" {...base} r={4} />
+          </g>
+        </svg>
+        <div className="gauge-value">
+          <p className="gauge-label">{label}</p>
+          {showValue && (
+            <>
+              <ItemValue
+                engine={engine}
+                oid={oid}
+                digits={digits}
+                units={units}
+                threshold={threshold}
+                format_with={format_with}
+              />
+            </>
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default GaugeSphere;
