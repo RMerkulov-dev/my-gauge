23e04ae6997937d3658476b1e8d6839eead73148
RMerkulov <fotoromario@gmail.com>
2023-09-26 22:51:40 +0300

    upd jest

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,7 @@
+import App from "../App";
+import { render } from "@testing-library/react";
+import { Eva } from "@eva-ics/webengine";
+
+test("Renders main page correctly", () => {
+  expect(true).toBeTruthy();
+});
